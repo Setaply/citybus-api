cf79c44f8a071599bf63c986151a374067f5150c
Setaply <contact.setaply@gmail.com>
2025-05-26 11:55:09 +0200

    Update vite.config.js

--- citybus-website/vite.config.js
+++ citybus-website/vite.config.js
@@ -1,5 +1,8 @@
+// vite.config.js
 export default {
-  build: {
-    sourcemap: true,
+  server: {
+    host: '0.0.0.0',       // Listen on all interfaces
+    port: 5173,            // Optional: specify the port
+    strictPort: true,      // Optional: fail if 5173 is taken
   }
 }
