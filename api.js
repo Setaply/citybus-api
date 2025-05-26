db2d1c1b9df717a45ec7c512b34ef3e57747fde3
Setaply <contact.setaply@gmail.com>
2025-05-26 09:12:55 +0200

    Update api.js

--- api.js
+++ api.js
@@ -10,12 +10,17 @@ let latitude = 0;
 api.use(express.json());
 
 api.get('/get-gps', async (req, res) => {
+  console.log("POST Request")
+  console.log({ longitude, latitude })
   res.json({ longitude, latitude });
 });
 
 api.post('/post-gps', async (req, res) => {
+  console.log("POST Request")
   const lon = parseFloat(req.query.longitude);
   const lat = parseFloat(req.query.latitude);
+  console.log(lon)
+  console.log(lat)
   if (!isNaN(lon)) longitude = lon;
   if (!isNaN(lat)) latitude = lat;
   res.json({ status: "GPS data updated" });
